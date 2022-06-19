65e4fe8b759c656d7a92be7e0697de37adec696c
Amon I <101267106+horseking82@users.noreply.github.com>
2022-06-20 00:25:08 +0200

    updated skillset page

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,6 +24,7 @@ let header = $(`
 <div class="collapse navbar-collapse " id="navbarSupportedContent">
   <ul class="navbar-nav ml-auto" id = "navbar-content">
    <li class="nav-item nav-item-hover"><a class="nav-link" href="index.html">Home</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="techstack.html">Skills</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
